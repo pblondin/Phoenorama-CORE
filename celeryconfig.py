60c10a64a04bbb0f1c15dc2f8b9dccfa25badd04
Philippe Blondin <pblondin@phoenorama.org>
2012-03-29 13:24:25 -0400

    fix imports and add phoenorama.org broker

--- celeryconfig.py
+++ celeryconfig.py
@@ -4,7 +4,7 @@ Created on Mar 17, 2012
 @author: r00tme
 '''
 # List of modules to import when celery starts.
-CELERY_IMPORTS = ("scanner.tasks", )
+CELERY_IMPORTS = ("scanner.openvas.tasks", )
 
 ## Result store settings.
 CELERY_RESULT_BACKEND = "amqp"
@@ -12,7 +12,8 @@ CELERY_RESULT_BACKEND = "amqp"
 
 ## Broker settings.
 #BROKER_URL = "amqp://guest:guest@localhost:5672//"
-BROKER_URL = "amqp://guest:guest@10.0.1.16:5672//"
+BROKER_URL = "amqp://guest:guest@phoenorama.org:5672//"
+#BROKER_URL = "amqp://guest:guest@10.0.1.16:5672//"
 
 ## Worker settings
 ## If you're doing mostly I/O you can have more processes,
