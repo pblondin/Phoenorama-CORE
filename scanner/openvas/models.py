a57d54021050e755e129c300fbf40a11a30580fd
Philippe Blondin <blondin.philippe@gmail.com>
2012-04-03 16:19:07 -0400

    Add _id field

--- scanner/openvas/models.py
+++ scanner/openvas/models.py
@@ -30,6 +30,7 @@ Created on Mar 29, 2012
 
 class Openvas():
     def __init__(self):
+        self._id = ''
         self.task_uuid = ''
         self.name = ''
         self.description = ''
@@ -38,7 +39,8 @@ class Openvas():
         self.status = ''
         
     def toJSON(self):
-        json = {'task_uuid' : self.task_uuid,
+        json = {'_id': self._id,
+                'task_uuid' : self.task_uuid,
                 'report_uuid' : self.report_uuid,
                 'name': self.name,
                 'description': self.description,
