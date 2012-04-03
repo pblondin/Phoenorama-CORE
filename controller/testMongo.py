c0b6a73ecd67b35f27ab87a14a01196a05502ab4
Philippe Blondin <pblondin@phoenorama.org>
2012-04-02 21:24:05 -0400

    Test case for MongoDB storage

---
+++ controller/testMongo.py
@@ -0,0 +1,19 @@
+'''
+Created on Mar 29, 2012
+
+@author: r00tmac
+'''
+from scanner.openvas.parser import parseXML
+from pymongo import Connection
+
+if __name__ == '__main__':
+    openvas_xml_report = file('../docs/report-samples/openvas-2hosts-2012_03_24.xml', 'r')
+    
+    report = parseXML(openvas_xml_report)
+    #print report.printFullReport()
+    
+    connection = Connection()
+    openvasReport = connection.phoenorama.openvasReport
+    
+    openvasReport.insert(report.toJSON())
+    
