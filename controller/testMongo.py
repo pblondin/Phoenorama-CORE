e35454baad5c4407082d08ff9b38f618ccd5e490
Philippe Blondin <blondin.philippe@gmail.com>
2012-04-03 13:44:59 -0400

    Fix imports

--- controller/testMongo.py
+++ controller/testMongo.py
@@ -3,13 +3,13 @@ Created on Mar 29, 2012
 
 @author: r00tmac
 '''
-from scanner.openvas.parser import parseXML
+from scanner.openvas.parser import parse
 from pymongo import Connection
 
 if __name__ == '__main__':
     openvas_xml_report = file('../docs/report-samples/openvas-2hosts-2012_03_24.xml', 'r')
     
-    report = parseXML(openvas_xml_report)
+    report = parse(openvas_xml_report)
     #print report.printFullReport()
     
     connection = Connection()
