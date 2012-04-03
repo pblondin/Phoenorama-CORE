887c96f809262b8f7f394e93490cc581258f93b8
Philippe Blondin <pblondin@phoenorama.org>
2012-04-02 21:22:07 -0400

    Testing saveReport task

--- controller/testTask.py
+++ controller/testTask.py
@@ -5,7 +5,7 @@ Created on Mar 24, 2012
 '''
 
 from scanner.openvas.model import Openvas
-from scanner.openvas.tasks import run
+from scanner.openvas.tasks import run, saveReport
 
 
 if __name__ == '__main__':
@@ -14,6 +14,9 @@ if __name__ == '__main__':
     o.name = "Basic test"
     o.description = "Openvas scan against localhost"
     o.target = "localhost"
-    result = run.delay(o.target) #@UndefinedVariable - prevent PyDev error
+    #result = run.delay(o.target) #@UndefinedVariable - prevent PyDev error
     #o.task_uuid, o.report_uuid = result.wait()
+    #print result.wait()
+    
+    result = saveReport.delay("e41c81ea-24e4-4981-a0d0-1b5d9839fbb8") #@UndefinedVariable
     print result.wait()
