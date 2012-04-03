85666214dec62480f18e7b2173512e8fdbbd19f8
Philippe Blondin <blondin.philippe@gmail.com>
2012-04-02 22:59:43 -0400

    Create tests functions

--- controller/testTask.py
+++ controller/testTask.py
@@ -6,10 +6,9 @@ Created on Mar 24, 2012
 
 from scanner.openvas.models import Openvas
 from scanner.openvas.tasks import run, saveReport
+from scanner.nmap.tasks import run
 
-
-if __name__ == '__main__':
-
+def testOpenVas():
     o = Openvas()
     o.name = "Basic test"
     o.description = "Openvas scan against localhost"
@@ -17,6 +16,17 @@ if __name__ == '__main__':
     #result = run.delay(o.target) #@UndefinedVariable - prevent PyDev error
     #o.task_uuid, o.report_uuid = result.wait()
     #print result.wait()
-    
+        
     result = saveReport.delay("e41c81ea-24e4-4981-a0d0-1b5d9839fbb8") #@UndefinedVariable
     print result.wait()
+        
+def testNmap():
+    result = run.delay("localhost") #@UndefinedVariable
+    print result.wait()
+
+
+if __name__ == '__main__':
+
+    #testOpenVas()
+    testNmap()
+    
