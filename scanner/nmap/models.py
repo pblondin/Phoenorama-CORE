64ac3ee5e90bd8434dfc5657294fc7d4923b6cc9
Philippe Blondin <blondin.philippe@gmail.com>
2012-04-06 11:39:45 -0400

    Add printing and toJSON methods

--- scanner/nmap/models.py
+++ scanner/nmap/models.py
@@ -27,7 +27,7 @@ Created on Apr 2, 2012
 @author: r00tme
 '''
 class Nmap():
-    def __init__(self):
+    def __init__(self, task_uuid=''):
         self.task_uuid = ''
         self.name = ''
         self.description = ''
@@ -53,3 +53,49 @@ class Report():
                                  
     def getHosts(self):
         return self.results_by_host.keys()
+    
+    def printSummary(self):
+        buf = ""
+        for hostname in self.getHosts():
+            buf += "Host: " + hostname + "\n"
+            buf += "\tNumber of open ports: " + str(len(self.results_by_host[hostname])) + "\n"
+        return buf
+        
+    def printHostResult(self, hostname):
+        buf = "Host: " + hostname + "\n"
+        buf += "\nList of open ports: \n"
+        for result in self.results_by_host[hostname]:
+            if result['portid']:
+                buf += "\tPortID: " + result['portid'] + "\n"
+            if result['service']:
+                buf += "\tService: " + result['service'] + "\n"
+            if result['product']:
+                buf += "\tProduct: " + result['product'] + "\n"
+            if result['version']:
+                buf += "\tVersion: " + result['version'] + "\n"
+            if result['scriptid']:
+                buf += "\tScriptID: " + result['scriptid'] + "\n"
+            if result['output']:
+                buf += "\tOutput: " + result['output'] + "\n"
+            buf += "\n"
+        return buf
+    
+    def printFullReport(self):
+        buf = "Report summary\n"
+        buf += "----------------------\n"
+        buf += self.printSummary() + "\n"
+        buf += "Detailed report\n"
+        buf += "----------------------\n"
+        for hostname in self.getHosts():
+            buf += self.printHostResult(hostname) + "\n"
+        return buf
+    
+    def __str__(self):
+        return self.printSummary()
+    
+    def toJSON(self):
+        json = {'report_uuid' : self.reportUuid,
+                'scan_info' : self.scan_info,
+                'results_by_host': self.results_by_host
+                }
+        return json
