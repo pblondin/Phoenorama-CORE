f973f0a242698854f61626f5c1e35c1d29260aa8
Philippe Blondin <blondin.philippe@gmail.com>
2012-04-06 11:40:04 -0400

    Parse NMAP Xml report

--- scanner/nmap/parser.py
+++ scanner/nmap/parser.py
@@ -26,13 +26,14 @@ Created on Apr 2, 2012
 
 @author: r00tme
 '''
+import uuid
 from lxml import etree
 from base64 import b64encode
 from models import Report
 
 def parse(document):
     root = etree.parse(document)    
-    report = Report() # @TODO generated report-uuid
+    report = Report(uuid.uuid4())
       
     # Fill the scan_info dictionary
     report.scan_info['scan_start'] = root.xpath('//@startstr')[0]
@@ -41,11 +42,40 @@ def parse(document):
     report.scan_info['version'] = root.xpath('//@version')[0]
     report.scan_info['extrainfo'] = root.xpath('//@summary')[0]
 
-    # iterate over ports
-    for result in root.xpath('//ports/port'):
-        print result[0].tag
+    # iterate over host
+    for result in root.xpath('//host'):
+        
+        #@TODO : add safety net
+        hostname =  b64encode(result.xpath('hostnames/hostname[@type = "user"]/@name')[0]) # hostname has to be base64 encode to prevent "." in key (mongodb issue)
+
+        # check if result already exist for the hostname
+        if not report.results_by_host.has_key(hostname):
+            report.results_by_host[hostname] = []
+
+        # iterate over ports
+        for port in result.xpath('ports/port'):
+            vuln = {}
+            
+            vuln['portid'] = port.xpath('@portid')[0]
+            vuln['service'] = port.xpath('service/@name')[0]
+            vuln['product'] = port.xpath('service/@product')[0]
+            vuln['version'] = port.xpath('service/@version')[0]
+            
+            # get script info
+            vuln['scriptid'] = port.xpath('script/@id')[0]
+            vuln['output'] = port.xpath('script/@output')[0]
+            
+            # append vulnerability to results list
+            report.results_by_host[hostname].append(vuln)
+
+    return report
 
 if __name__ == '__main__':
     nmap_xml_report = file('../../docs/report-samples/nmap-localhost-2012_04_02.xml', 'r')
     report = parse(nmap_xml_report)
-    #print report.printFullReport()
+    print report.printFullReport()
+    
+    
+    
+    
+    
