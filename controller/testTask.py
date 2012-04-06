58aa9c2e44bfd9fb3ad1c5a746ffcb18d8e4bf35
Philippe Blondin <pblondin@phoenorama.org>
2012-04-06 09:39:13 -0400

    New test case

--- controller/testTask.py
+++ controller/testTask.py
@@ -5,28 +5,36 @@ Created on Mar 24, 2012
 '''
 
 from scanner.openvas.models import Openvas
-from scanner.openvas.tasks import run, saveReport
-from scanner.nmap.tasks import run
+from scanner.openvas.tasks import run, save
+#from scanner.nmap.tasks import run
 
 def testOpenVas():
     o = Openvas()
     o.name = "Basic test"
     o.description = "Openvas scan against localhost"
-    o.target = "localhost"
-    #result = run.delay(o.target) #@UndefinedVariable - prevent PyDev error
+    #o.target = "localhost"
+    o.target = "www.rdexperts.com"
+    o.status = "NEW"
+    save.delay(o) #@UndefinedVariable
+    
+    run.delay(o) #@UndefinedVariable
+    
+    
+    #task_uuid, report_uuid = run.delay(o).wait() #@UndefinedVariable - prevent PyDev error
     #o.task_uuid, o.report_uuid = result.wait()
     #print result.wait()
+    #print task_uuid
         
-    result = saveReport.delay("e41c81ea-24e4-4981-a0d0-1b5d9839fbb8") #@UndefinedVariable
-    print result.wait()
+    #result = saveReport.delay("e41c81ea-24e4-4981-a0d0-1b5d9839fbb8") #@UndefinedVariable
+    #print result.wait()
         
 def testNmap():
-    result = run.delay("localhost") #@UndefinedVariable
+    result = run.delay("www.rdexperts.com") #@UndefinedVariable
     print result.wait()
 
 
 if __name__ == '__main__':
 
-    #testOpenVas()
-    testNmap()
+    testOpenVas()
+    #testNmap()
     
